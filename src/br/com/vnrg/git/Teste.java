3075dc610535b716278f1997511160e74f269036
Vinicius N Ribeiro Garcia <viniciusnrgarcia@gmail.com>
2015-01-26 00:23:44 -0200

    Criacao da primeira versao do projeto test-git

---
+++ src/br/com/vnrg/git/Teste.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.vnrg.git;
+
+/**
+ * @author Vinicius Garcia
+ *
+ */
+public class Teste {
+	
+	/**
+	 * Método main.
+	 * @param args String [].
+	 */
+	public static void main (String [] args) {
+		System.out.println("Teste 1");
+	}
+
+}
